7a0dbebd5e7ace9db4c1efe56a209bbe8e0c6534
jimhantrix <jimtekanyo@gmail.com>
2017-02-16 20:20:17 -0800

    first commit

---
+++ app/models/article.rb
@@ -0,0 +1,6 @@
+class Article < ApplicationRecord
+
+	 belongs_to :user
+  has_many :votes
+  has_many :users, through: :votes
+end
---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+
+  has_secure_password
+
+  has_many :votes
+  has_many :articles, through: :votes
+
+end
